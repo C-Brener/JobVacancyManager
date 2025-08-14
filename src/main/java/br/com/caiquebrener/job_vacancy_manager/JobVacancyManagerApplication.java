fe6fb0a9f39135950d81c17534edd18d67659687
caique-brener <brenercaique0806@gmail.com>
2025-08-13 23:06:22 -0300

    feat: init project

---
+++ src/main/java/br/com/caiquebrener/job_vacancy_manager/JobVacancyManagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.caiquebrener.job_vacancy_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobVacancyManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobVacancyManagerApplication.class, args);
+	}
+
+}
