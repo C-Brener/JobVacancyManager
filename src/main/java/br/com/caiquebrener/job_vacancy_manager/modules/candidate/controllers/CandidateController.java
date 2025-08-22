abea772db812e35bc55a9a65e1d2f1c3f219e771
caique-brener <brenercaique0806@gmail.com>
2025-08-21 21:24:29 -0300

    feat: adjusting controller to use useCase

--- src/main/java/br/com/caiquebrener/job_vacancy_manager/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/caiquebrener/job_vacancy_manager/modules/candidate/controllers/CandidateController.java
@@ -1,7 +1,10 @@
 package br.com.caiquebrener.job_vacancy_manager.modules.candidate.controllers;
 
 import br.com.caiquebrener.job_vacancy_manager.modules.candidate.models.CandidateEntity;
+import br.com.caiquebrener.job_vacancy_manager.modules.candidate.usecases.CreateCandidateUseCase;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,8 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/candidate/")
 public class CandidateController {
 
+    @Autowired
+    private CreateCandidateUseCase createCandidateUseCase;
+
     @PostMapping
-    public void create(@Valid @RequestBody CandidateEntity candidate) {
-        System.out.println("Candidate " + candidate);
+    public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidate) {
+        var result = createCandidateUseCase.execute(candidate);
+        return ResponseEntity.ok(result);
     }
 }
