eec9048efd4fe54893348a671b2efc0c88cca3d9
caique-brener <brenercaique0806@gmail.com>
2025-09-22 07:53:54 -0300

    feat: create swagger documentation

--- src/main/java/br/com/caiquebrener/job_vacancy_manager/modules/company/dto/CreateJobDTO.java
+++ src/main/java/br/com/caiquebrener/job_vacancy_manager/modules/company/dto/CreateJobDTO.java
@@ -1,11 +1,14 @@
 package br.com.caiquebrener.job_vacancy_manager.modules.company.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class CreateJobDTO {
-
+    @Schema(example = "We are looking for a skilled Android Developer to join our mobile team.", requiredMode = Schema.RequiredMode.REQUIRED)
     private String description;
+    @Schema(example = "Mid Level", requiredMode = Schema.RequiredMode.REQUIRED)
     private String level;
+    @Schema(example = "Health insurance, meal voucher, home office support", requiredMode = Schema.RequiredMode.REQUIRED)
     private String benefits;
 }
